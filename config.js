4bb1adac82aa9891531986dcef77d1866d225728
botskaking <152233761+botskaking@users.noreply.github.com>
2024-03-12 16:55:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,17 +7,17 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['234704535241', 'EXCEL', true], 
-  ['2348021779696', 'EXCEL', true], 
-  ['234704503541', '⚡Excel', true],
+  ['918279975767', 'EXCEL', true], 
+  ['918279975767', 'EXCEL', true], 
+  ['918279975767', '⚡Excel', true],
 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2341', '2348021779696', '2347045035241','233533763772'] 
-global.prems = ['2347045035241','233208179431','233533763772']
-global.allowed = ['2348021779696', '2347045035241']
+global.mods = ['918279975767', '918279975767', '918279975767','233533763772'] 
+global.prems = ['918279975767','918279975767','918279975767']
+global.allowed = ['918279975767', '918279975767']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -52,14 +52,14 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'STAR-MD-V2'
+global.botname = 'ZERO-MD'
 global.premium = 'true'
-global.packname = 'STAR-MD' 
-global.author = '@Excel' 
+global.packname = 'ZERO-MD' 
+global.author = '@ironman' 
 global.menuvid = 'https://i.imgur.com/BPqmxsm.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
 global.dygp='▢✓ Follow My channel\nhttps://chat.whatsapp.com/Lg0lY4M1k8oDMYzylg86xs\n'
-global.fgsc = 'https://github.com/Xcelsama/STAR-MD-V2' 
+global.fgsc = 'https://github.com/botskaking/STAR-MD-V2' 
 global.fgyt = 'https://chat.whatsapp.com/EmP3syvou18HrZk6R6nTAK'
 global.fgpyp = 'https://chat.whatsapp.com/EmP3syvou18HrZk6R6nTAK'
 global.fglog = 'STAR.jpg' 
